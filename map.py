bb20e5d60f0f0400243b3476ea0461a80db7c626
Adéla Kubíková <kubikade@fel.cvut.cz>
2020-07-27 16:32:07 +0200

    plotting functions

--- map.py
+++ map.py
@@ -1,34 +1,12 @@
 import gmplot
-import reader
-import utils
 
-granularity = 25
-vehicles = []
 
-filepath = "pneuma_sample_dataset/pneuma_sample_dataset.csv"
-
-BBoxleft = [37.99130, 37.99163, 23.73236, 23.73326]
-BBoxright = [37.99083, 37.99102, 23.73559, 23.73694]
-BBoxdown = [37.99134, 37.99157, 23.72996, 23.73061]
-
-vehicles = reader.load_rows_in_interval(filepath, 1, 150, granularity)
-ll = utils.pass_through_BBox(vehicles, *BBoxleft)
-rr = utils.pass_through_BBox(ll, *BBoxright)
-dep = utils.pass_through_BBox(rr, *BBoxdown)
-"""
-vehicle = reader.load_one_row(filepath, 65, granularity)
-dep = []
-dep.append(vehicle)
-"""
-
-centerLon = (float(dep[0].findMinLon()) + float(dep[0].findMaxLon())) / 2
-centerLat = (float(dep[0].findMinLat()) + float(dep[0].findMaxLat())) / 2
-gmap = gmplot.GoogleMapPlotter(centerLat, centerLon, 17)
-
-for vehicle in dep:
-    print(vehicle)
-    gmap.scatter(vehicle.lat_list, vehicle.lon_list, '#00FF00', size=3, marker=False)
-    gmap.plot(vehicle.lat_list, vehicle.lon_list, 'blue', edge_width=2.5)
-
-gmap.draw("map.html")
+def to_map_plot(vehicles, filename="map.html"):
+    centerLon = (float(vehicles[0].findMinLon()) + float(vehicles[0].findMaxLon())) / 2
+    centerLat = (float(vehicles[0].findMinLat()) + float(vehicles[0].findMaxLat())) / 2
+    gmap = gmplot.GoogleMapPlotter(centerLat, centerLon, 17)
+    for vehicle in vehicles:
+        gmap.scatter(vehicle.lat_list, vehicle.lon_list, '#00FF00', size=3, marker=False)
+        gmap.plot(vehicle.lat_list, vehicle.lon_list, 'blue', edge_width=2.5)
+    gmap.draw(filename)
 
--- plot.py
+++ plot.py
@@ -1,5 +1,4 @@
 import utils
-import reader
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
@@ -7,15 +6,15 @@ from matplotlib.collections import LineCollection
 
 
 def get_distance_list(vehicle):
-    distanceList = []
+    distance_list = []
     for i in range(len(vehicle.lat_list)):
         j = 0
         dist = 0.0
         while j <= i:
             dist += utils.get_distance(vehicle, j)
             j += 1
-        distanceList.append(dist)
-    return distanceList
+        distance_list.append(dist)
+    return distance_list
 
 
 def get_segments(time_list, distance_list):
@@ -24,66 +23,41 @@ def get_segments(time_list, distance_list):
     return segments
 
 
-filepath = "pneuma_sample_dataset/pneuma_sample_dataset.csv"
-granularity = 25
-vehicles = []
-numbers = " "
-BBox = [37.98948, 37.99054, 23.73059, 23.73113]
+def find_minimum_maximum(vehicles):
+    min_dist = min(get_distance_list(vehicles[0]))
+    max_dist = max(get_distance_list(vehicles[0]))
+    min_time = min(vehicles[0].time_list)
+    max_time = max(vehicles[0].time_list)
+    min_speed = min(vehicles[0].speed_list)
+    max_speed = max(vehicles[0].speed_list)
+    for vehicle in vehicles:
+        distance_list = get_distance_list(vehicle)
+        min_speed = min(vehicle.speed_list) if min(vehicle.speed_list) < min_speed else min_speed
+        max_speed = max(vehicle.speed_list) if max(vehicle.speed_list) > max_speed else max_speed
+        min_dist = min(distance_list) if min(distance_list) < min_dist else min_dist
+        max_dist = max(distance_list) if max(distance_list) > max_dist else max_dist
+        min_time = min(vehicle.time_list) if min(vehicle.time_list) < min_time else min_time
+        max_time = max(vehicle.time_list) if max(vehicle.time_list) > max_time else max_time
+    return min_dist, max_dist, min_time, max_time, min_speed, max_speed
 
-BBoxleft = [37.99130, 37.99163, 23.73236, 23.73326]
-BBoxright = [37.99083, 37.99102, 23.73559, 23.73694]
-BBoxdown = [37.99134, 37.99157, 23.72996, 23.73061]
-#2478 max index
 
-#vehicles = reader.load_rows_in_interval(filepath, 150, 1860, granularity)
-#dep = utils.depart_from_BBox(vehicles, *BBox)
-"""
-vehicles = reader.load_rows_in_interval(filepath, 370, 400, granularity)
-ll = utils.pass_through_BBox(vehicles, *BBoxleft)
-rr = utils.pass_through_BBox(ll, *BBoxright)
-dep = utils.pass_through_BBox(rr, *BBoxdown)
-"""
-vehicle = reader.load_one_row(filepath, 66, granularity)
-dep = []
-dep.append(vehicle)
-
-fig, ax = plt.subplots()
-
-mindist = min(get_distance_list(dep[0]))
-maxdist = max(get_distance_list(dep[0]))
-mintime = min(dep[0].time_list)
-maxtime = max(dep[0].time_list)
-minspeed = min(dep[0].speed_list)
-maxspeed = max(dep[0].speed_list)
-
-for vehicle in dep:
-    print(vehicle)
-    distance_list = get_distance_list(vehicle)
-    minspeed = min(vehicle.speed_list) if min(vehicle.speed_list) < minspeed else minspeed
-    maxspeed = max(vehicle.speed_list) if max(vehicle.speed_list) > maxspeed else maxspeed
-    mindist = min(distance_list) if min(distance_list) < mindist else mindist
-    maxdist = max(distance_list) if max(distance_list) > maxdist else maxdist
-    mintime = min(vehicle.time_list) if min(vehicle.time_list) < mintime else mintime
-    maxtime = max(vehicle.time_list) if max(vehicle.time_list) > maxtime else maxtime
-
-norm = plt.Normalize(minspeed, maxspeed)
-cmap = 'coolwarm'
-lines = []
-
-for vehicle in dep:
-    seg = get_segments(vehicle.time_list, get_distance_list(vehicle))
-    lc = LineCollection(seg, cmap=cmap, norm=norm)
-    lc.set_array(np.array(vehicle.speed_list))
-    lc.set_linewidth(2)
-    lc.set_label(vehicle.track_id + " " + vehicle.type)
-    line = ax.add_collection(lc)
-
-plt.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), label="velocity [km/h]")
-plt.ylabel("distance [m]")
-plt.xlabel("time [s]")
-plt.grid()
-plt.legend(loc="upper left")
-ax.set_xlim(mintime, maxtime)
-ax.set_ylim(mindist, maxdist)
-plt.show()
-
+def to_plot(vehicles):
+    min_dist, max_dist, min_time, max_time, min_speed, max_speed = find_minimum_maximum(vehicles)
+    fig, ax = plt.subplots()
+    norm = plt.Normalize(min_speed, max_speed)
+    colormap = 'coolwarm'
+    for vehicle in vehicles:
+        seg = get_segments(vehicle.time_list, get_distance_list(vehicle))
+        lc = LineCollection(seg, cmap=colormap, norm=norm)
+        lc.set_array(np.array(vehicle.speed_list))
+        lc.set_linewidth(2)
+        lc.set_label(vehicle.track_id + " " + vehicle.type)
+        ax.add_collection(lc)
+    plt.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=colormap), label="velocity [km/h]")
+    plt.ylabel("distance [m]")
+    plt.xlabel("time [s]")
+    plt.grid()
+    plt.legend(loc="upper left")
+    ax.set_xlim(min_time, max_time)
+    ax.set_ylim(min_dist, max_dist)
+    plt.show()
