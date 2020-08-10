61aebb7ddc5be8fd67557e4b8c78cc8c209f20c7
Adéla Kubíková <kubikade@fel.cvut.cz>
2020-08-10 20:07:23 +0200

    Add script to plot multiple trajectories

---
+++ scripts/plot_multiple_vehicles_movingpandas.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("..")
+import movingpandas as mpd
+import warnings
+warnings.simplefilter("ignore")
+import reader
+import reader_pandas
+
+
+filepath = "../pneuma_sample_dataset/pneuma_sample_dataset.csv"
+loaded_vehicles = reader.load_multiple_rows(filepath, 20)
+all = [i for i in range(20)]
+
+df = reader_pandas.create_df(loaded_vehicles)
+gdf = reader_pandas.create_gdf(df, all)
+MIN_LENGTH = 100 # meters
+traj_collection = mpd.TrajectoryCollection(gdf, 'track_id', min_length=MIN_LENGTH)
+print("Finished creating {} trajectories".format(len(traj_collection)))
+traj_collection.plot()
+
+plt.show()
