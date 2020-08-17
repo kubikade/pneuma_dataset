0acbde65ef94c35d0892b874e924117eafe16761
Adéla Kubíková <kubikade@fel.cvut.cz>
2020-08-17 17:36:11 +0200

    plot with basemap

--- playground.py
+++ playground.py
@@ -1,10 +1,4 @@
-import urllib
-import os
-import numpy as np
-import pandas as pd
-from geopandas import GeoDataFrame, read_file
-from shapely.geometry import Point, LineString, Polygon
-from datetime import datetime, timedelta
+from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
 
 import sys
@@ -15,7 +9,7 @@ warnings.simplefilter("ignore")
 import reader
 import reader_pandas
 import utils
-
+import geopandas as gpd
 import contextily as ctx
 
 
@@ -23,11 +17,20 @@ filepath = "pneuma_sample_dataset/pneuma_sample_dataset.csv"
 loaded_vehicles = reader.load_multiple_rows(filepath, 200)
 all = [i for i in range(200)]
 
-df = reader_pandas.create_df(loaded_vehicles)
-print(df)
-print(len(df.index))
+poly = Polygon([(23.73459, 37.99090), (23.73444, 37.99020), (23.73468, 37.99090), (23.73450, 37.99032)])
+poly4 = Polygon([(23.73250, 37.99112), (23.73250, 37.99104), (23.73456, 37.99079), (23.73456, 37.99071)])
 
-gdf = reader_pandas.create_gdf_from_whole_df(df)
+filtered = utils.veh_pass_through_polygons(loaded_vehicles, [poly, poly4])
+print("Found {} intersections".format(len(filtered)))
+
+
+df = reader_pandas.create_df(filtered)
+#print(df)
+
+gdf = reader_pandas.create_gdf_using_lists(df)
+print(gdf)
+
+"""
 MIN_LENGTH = 100 # meters
 traj_collection = mpd.TrajectoryCollection(gdf, 'track_id', min_length=MIN_LENGTH)
 print("Finished creating {} trajectories".format(len(traj_collection)))
@@ -42,15 +45,16 @@ poly3 = Polygon([(23.73217, 37.99069), (23.73217, 37.99055), (23.73359, 37.99051
 #ioulianou
 poly4 = Polygon([(23.73250, 37.99112), (23.73250, 37.99104), (23.73456, 37.99079), (23.73456, 37.99071)])
 
-# intersecting = utils.pass_through_Polygon(traj_collection, poly)
-#intersecting = utils.pass_through_Polygon(traj_collection, poly)
-#intersecting = utils.pass_through_Polygon(intersecting, poly4)
 
-intersecting = utils.pass_through_Polygons(traj_collection, [poly, poly4])
+intersecting = utils.traj_through_polygons(traj_collection, [poly, poly4])
 print("Found {} intersections".format(len(intersecting)))
-
+"""
 gdf = gdf.to_crs(epsg=3857)
-ax = intersecting.plot()
+
+ax = gdf.plot(figsize=(10, 10), alpha=0.5, edgecolor='k')
+ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik)
+
+#ax = traj_collection.plot()
+plt.show()
 
 # traj_collection.hvplot(geo=True, tiles='OSM', line_width=5, frame_width=300, frame_height=300)
-plt.show()
